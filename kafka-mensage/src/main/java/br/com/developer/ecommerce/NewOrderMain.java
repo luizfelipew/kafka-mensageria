0e21513ba4c5cb6b5b469e1890d64f852f30fd7a
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-02-10 17:15:19 -0300

    Paralelizando e a importância das keys

--- kafka-mensage/src/main/java/br/com/developer/ecommerce/NewOrderMain.java
+++ kafka-mensage/src/main/java/br/com/developer/ecommerce/NewOrderMain.java
@@ -8,26 +8,32 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Objects;
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
-        var value = "132123,67523,7894589745";
-        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println(
-                "Sucesso enviando " + data.topic() + " :::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        };
-        var email = "Thank you for your order! We are processing your order!";
-        var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", email, email);
-        producer.send(record, callback).get();
-        producer.send(emailRecord, callback).get();
+
+        for (int i = 0; i < 100; i++) {
+            var key = UUID.randomUUID().toString();
+            var value = key + ",67523,7894589745";
+            var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", key, value);
+            Callback callback = (data, ex) -> {
+                if (ex != null) {
+                    ex.printStackTrace();
+                    return;
+                }
+                System.out.println(
+                    "Sucesso enviando " + data.topic() + " :::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+            };
+            var email = "Thank you for your order! We are processing your order!";
+            var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", key, email);
+            producer.send(record, callback).get();
+            producer.send(emailRecord, callback).get();
+        }
+
     }
 
     private static Properties properties() {
