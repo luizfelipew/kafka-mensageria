2a053da7cb0bd0b4e748e6236ac6e097590a0b1a
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-02-11 17:05:23 -0300

    Serialização com GSON

---
+++ kafka-mensage/src/main/java/br/com/developer/ecommerce/GsonSerializer.java
@@ -0,0 +1,16 @@
+package br.com.developer.ecommerce;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.kafka.common.serialization.Serializer;
+
+public class GsonSerializer<T> implements Serializer<T> {
+
+    private final Gson gson = new GsonBuilder().create();
+
+    @Override
+    public byte[] serialize(final String s, final T object) {
+        return gson.toJson(object).getBytes();
+    }
+
+}
--- kafka-mensage/src/main/java/br/com/developer/ecommerce/NewOrderMain.java
+++ kafka-mensage/src/main/java/br/com/developer/ecommerce/NewOrderMain.java
@@ -1,19 +1,24 @@
 package br.com.developer.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
-            for (int i = 0; i < 10; i++) {
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (int i = 0; i < 10; i++) {
 
-                var key = UUID.randomUUID().toString();
-                var value = key + ",67523,7894589745";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
-                var email = "Thank you for your order! We are processing your order!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+                    var email = "Thank you for your order! We are processing your order!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
