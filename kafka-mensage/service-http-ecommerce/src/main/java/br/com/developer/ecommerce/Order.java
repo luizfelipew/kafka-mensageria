0ab9be0517f5c9a57b8a37379d2a854a558c793d
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-09-28 19:26:09 -0300

    Usando um servidor http como ponto de entrada

---
+++ kafka-mensage/service-http-ecommerce/src/main/java/br/com/developer/ecommerce/Order.java
@@ -0,0 +1,17 @@
+package br.com.developer.ecommerce;
+
+import java.math.BigDecimal;
+
+public class Order {
+
+    private final String orderId;
+    private final BigDecimal amount;
+    private final String email;
+
+    public Order(final String orderId, final BigDecimal amount, final String email) {
+        this.orderId = orderId;
+        this.amount = amount;
+        this.email = email;
+    }
+
+}
